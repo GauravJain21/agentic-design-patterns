83e0e0d93591deb6c055c61f030c0d4d526cfdff
Gaurav Jain <gauravjainhere@google.com>
2025-09-25 08:08:15 +0000

    Add prompt chaining

---
+++ chapter_1/prompt_chaining.py
@@ -0,0 +1,8 @@
+"""
+import os
+from langchain_google_vertexai import ChatVertexAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from dotenv import load_dotenv
+"""
